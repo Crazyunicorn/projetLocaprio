369cae1ed4385c17ca923103eebd58a0754b7583
Marie-Léa Vincent <marielea.vincent@gmail.com>
2018-11-17 11:37:24 +0100

    merge

--- appreact/src/Components/Pages/Description.jsx
+++ appreact/src/Components/Pages/Description.jsx
@@ -4,14 +4,14 @@ import Card from '../Elements/Card';
 
 
 
-className Description extends Component {
+class Description extends Component {
   constructor(){
     super();
     this.state = {appart:{}};
   }
 
   getOneAppart = () =>{
-    axios.get('http://localhost:5000/api/description/'+this.props.match.params.id)
+    axios.get('http://localhost:5000/api/description/'+this.props.match.params._id)
     .then(responseFromApi => {
       console.log(responseFromApi.data)
       this.setState({
@@ -19,37 +19,12 @@ className Description extends Component {
       })
     })
   }
+
+  
   render() {
     return(
-<div className="cardcloseup">
-<div className="tile is-parent">
-  <div className="tile is-3 is-vertical is-parent">
-    <div className="tile is-child box">
-      <p className="title">{{apparts.user.firstName}} {{apparts.user.lastName}}</p>
-<figure className="imageuser">
-            <img src="/images/rsz_julienbig.jpg" alt="Placeholder image" style="height:300px; width: 200px";>
-          </figure>
-            <ul>
-              <br>
-              <li>{{apparts.adresse.city}} {{apparts.adresse.zip_code}}</li>
-              <li> {{apparts.surface}}m2 - {{apparts.prix}}€</li>
-              </br> 
-            </br>
-              </div>
-    
-  </div>
-  <div className="tile is-parent">
-    <div className="tile is-child box">
-      <p className="title"></p>
-          <figure className="imageap">
-            <img src="/images/apartment.png" alt="Placeholder image">
-          </figure>
-          <br>
-          {{apparts.description}}
-          </br>
-    </div>
-  </div>
-</div>
+      <div>
+    <Card appart={this.state.appart}></Card>
 </div>
     );
   }
